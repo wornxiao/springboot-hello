2d0a42e5f2e44d6a2a886890bdb14af4841d0118
worn <894154899@qq.com>
2022-09-05 23:30:51 +0800

    Add files via upload
    hello jenkins

---
+++ src/main/java/com/xiao/jenkins/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.xiao.jenkins.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello")
+    @ResponseBody
+    public Object hello(){
+        return "hello jenkins!";
+    }
+}
