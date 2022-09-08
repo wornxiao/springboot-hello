af6ee9afdbcdd9a9f6e6a1c019489d9596f594ae
worn.xiao <worn.xiao@huolala.cn>
2022-09-08 22:02:59 +0800

    fix

--- src/main/java/com/xiao/jenkins/controller/HelloController.java
+++ src/main/java/com/xiao/jenkins/controller/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
     @RequestMapping(value = "/hello")
     @ResponseBody
     public Object hello(){
-        return "hello docker! groble over";
+        return "hello docker! 结束了!";
     }
 }
